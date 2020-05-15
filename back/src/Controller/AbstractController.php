1d866b9d1f7222013957637cda4c6bbd18d0d9bb
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-05-15 17:35:12 +0200

    remove dependency on twig

--- back/src/Controller/AbstractController.php
+++ back/src/Controller/AbstractController.php
@@ -9,37 +9,18 @@
 
 namespace App\Controller;
 
-use Twig\Environment;
-use Twig\Extension\DebugExtension;
-use Twig\Loader\FilesystemLoader;
-
 /**
  *
  */
 abstract class AbstractController
 {
-    /**
-     * @var Environment
-     */
-    protected $twig;
-
 
     /**
      *  Initializes this class.
      */
     public function __construct()
     {
-        $loader = new FilesystemLoader(APP_VIEW_PATH);
-        $this->twig = new Environment(
-            $loader,
-            [
-                'cache' => !APP_DEV,
-                'debug' => APP_DEV,
-            ]
-        );
-        $this->twig->addExtension(new DebugExtension());
-
-        if(isset($_POST)) {
+        if (isset($_POST)) {
             $this->jsonInput = json_decode(file_get_contents('php://input'), true);
         }
     }

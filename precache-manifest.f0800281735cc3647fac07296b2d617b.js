75307a003baad9ea3bfde3c03ef19cd07b580868
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-05-18 18:31:39 +0200

    Updates

---
+++ precache-manifest.f0800281735cc3647fac07296b2d617b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ff56100f9f1fed5d994790766e3192",
+    "url": "/wcssci/index.html"
+  },
+  {
+    "revision": "7dd4a25d957162919d41",
+    "url": "/wcssci/static/css/2.706b5165.chunk.css"
+  },
+  {
+    "revision": "d4b5452facd3fd875f0f",
+    "url": "/wcssci/static/css/main.c3ee4c86.chunk.css"
+  },
+  {
+    "revision": "7dd4a25d957162919d41",
+    "url": "/wcssci/static/js/2.d2356469.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/wcssci/static/js/2.d2356469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b5452facd3fd875f0f",
+    "url": "/wcssci/static/js/main.8ce415e9.chunk.js"
+  },
+  {
+    "revision": "8c548c55faf113a07e2f",
+    "url": "/wcssci/static/js/runtime-main.aac0a401.js"
+  }
+]);
